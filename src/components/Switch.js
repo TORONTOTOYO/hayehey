5c52bae675e2efebfd189ec6bd3f700dc821942c
TORONTOTOYO <cjf5671@gmail.com>
2024-09-10 23:36:18 +0800

    initial commit

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -291,7 +291,7 @@ const StyledWrapper = styled.div`
 
   /* Mobile View Adjustments */
   @media (max-width: 768px) {
-    height: 45vh; /* Adjust height for medium screens */
+    height: 40vh; /* Adjust height for medium screens */
     width: 85vw; /* Adjust width for medium screens */
     
     .btn-class-name {
@@ -306,7 +306,7 @@ const StyledWrapper = styled.div`
   }
 
   @media (max-width: 480px) {
-    height: 45vh; /* Further adjust height for small screens */
+    height: 40vh; /* Further adjust height for small screens */
     width: 80vw; /* Further adjust width for small screens */
     
     .btn-class-name {
